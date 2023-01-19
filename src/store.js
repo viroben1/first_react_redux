b636f0c2be6674d84b8fe98f97c1d94261050617
benjamin farmer <viroben2001@gmail.com>
2023-01-18 23:47:58 -0800

    added todo

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './features/counterSlice'
+import todoReducer from './features/todoSlice'
 
 export const store = configureStore({
     reducer: {
-        counter: counterReducer
+        counter: counterReducer,
+        todos: todoReducer
     }
 })
